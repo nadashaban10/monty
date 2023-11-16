25fe61cfaa33ea2a74a520badf73234748cbc00b
loda26 <mokhaled26498@gmail.com>
2023-11-16 10:45:07 -0800

    sub tools 2

--- st2.c
+++ st2.c
@@ -1,10 +1,10 @@
 #include "monty.h"
 
 /**
- *nop - Does nothing.
- *@stack: Pointer to a pointer pointing to top node of the stack.
- *@line_num: Interger representing the line number of of the opcode.
- */
+ * nop - Does nothing.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_num: Interger representing the line number of of the opcode.
+*/
 void nop(stack_t **stack, unsigned int line_num)
 {
 	(void)stack;
@@ -13,10 +13,10 @@ void nop(stack_t **stack, unsigned int line_num)
 
 
 /**
- *swap_nodes - Swaps the top two elements of the stack.
- *@stack: Pointer to a pointer pointing to top node of the stack.
- *@line_num: Interger representing the line number of of the opcode.
- */
+ * swap_nodes - Swaps the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_num: Interger representing the line number of of the opcode.
+*/
 void swap_nodes(stack_t **stack, unsigned int line_num)
 {
 	stack_t *buf;
@@ -34,10 +34,10 @@ void swap_nodes(stack_t **stack, unsigned int line_num)
 }
 
 /**
- *add_nodes - Adds the top two elements of the stack.
- *@stack: Pointer to a pointer pointing to top node of the stack.
- *@line_number: Interger representing the line number of of the opcode.
- */
+ * add_nodes - Adds the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+*/
 void add_nodes(stack_t **stack, unsigned int line_number)
 {
 	int sum;
